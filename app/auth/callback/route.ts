030b9e1a1c125b8cff2922484f10bc8d8fab11ed
Fuwa <toryaki.corp@gmail.com>
2024-11-23 19:27:37 +0100

    First init

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,14 +2,18 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies })
+    const cookieStore = cookies()
+    const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(requestUrl.origin)
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(new URL('/', requestUrl.origin))
 }
